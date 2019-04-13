d5982b6685cc9948faadf63951c73608a24b564b
Loke Carlsson <carlssonloke@gmail.com>
2019-04-13 16:58:48 +0200

    Add features

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -10,6 +10,20 @@
         <g-link to="/docs/quickstart" class="button primary button--large">Quickstart</g-link>
         <g-link to="/docs" class="button">Documentation</g-link>
       </p>
+      <div class="features">
+        <div class="feature">
+          <h2>Quick setup</h2>
+          <p>Get started quick with all basic functionality already added and configured.</p>
+        </div>
+        <div class="feature">
+          <h2>Powerful features</h2>
+          <p>Utilize the power of Vue.js and Gridsome for a powerful application.</p>
+        </div>
+        <div class="feature">
+          <h2>Perfect scores</h2>
+          <p>Gridsome brings perfect scored on Google Lighthouse out of the box.</p>
+        </div>
+      </div>
     </div>
   </Section>
 </template>
@@ -60,5 +74,38 @@
   font-size: 1.3rem;
   font-weight: 300;
   opacity: 0.8;
+}
+
+.features {
+  border-top: 1px solid #57cea04b;
+  padding: 1.2rem 0;
+  margin-top: 2.5rem;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: flex-start;
+  align-content: stretch;
+  justify-content: space-between;
+  width: 75%;
+  max-width: 650px;
+
+  .feature {
+    flex-grow: 1;
+    flex-basis: 30%;
+    max-width: 30%;
+
+    h2 {
+      font-size: 1.5rem;
+      font-weight: 600;
+      border-bottom: none;
+      padding-bottom: 0;
+      margin-bottom: 0.8rem;
+    }
+
+    p {
+      font-size: 1.3rem;
+      font-weight: 300;
+      opacity: 0.8;
+    }
+  }
 }
 </style>
